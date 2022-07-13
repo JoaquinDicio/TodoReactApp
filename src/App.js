88465109e67a320c438b0dad8edb5ceb17ff5f0e
Joaquin di cio <joacodicio@gmail.com>
2022-07-14 01:21:16 +0200

    aa

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Home from './Pages/Home';
 function App() {
   return (
     <div className="App">
-      <GeneralContext>
       <BrowserRouter>
+      <GeneralContext>
       <Routes>
         <Route path='/TodoReactApp' element={<Home/>}/>
         <Route path='/' element={<Home/>}/>
       </Routes>
-      </BrowserRouter>
       </GeneralContext>
+      </BrowserRouter>
     </div>
   );
 }
